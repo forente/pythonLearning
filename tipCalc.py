7f4ae60916da57a3f6c8d71f35970b6caf58de8a
forente <zerofox16@gmail.com>
2017-08-09 15:10:09 -0500

    added change

--- tipCalc.py
+++ tipCalc.py
@@ -1,5 +1,5 @@
-bill  = int(input("what is the bill? "))
-rate  = int(input("What is the tip precentage? "))
-tip  = bill * rate/100
+bill  = input("what is the bill? ")
+rate  = input("What is the tip precentage? ")
+tip  = float (bill) * float(rate)/100
 print("Your tip is $"+ str(tip))
-print("Your total is $ {0:.2f}".format(bill+tip))
+print("Your total is ${0:.2f}".format(float(bill)+ float(tip)))
