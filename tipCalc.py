e4627109c731852a74abbdc00243ba9d024de5a9
forente <zerofox16@gmail.com>
2017-08-19 03:50:56 -0500

    checks if input is a number

--- tipCalc.py
+++ tipCalc.py
@@ -1,5 +1,21 @@
-bill  = input("what is the bill? ")
-rate  = input("What is the tip precentage? ")
-tip  = float (bill) * float(rate)/100
+bill  = 0;
+rate  = 0;
+tip = 0;
+
+def getInput (text):
+    num = True
+    i = 0
+    while num:
+        try:
+            i = float(input(text))
+            num = False
+        except ValueError:
+            num = True
+            print("Please enter a number. ")
+    return i
+
+bill  = getInput("what is the bill? ")
+rate  = getInput("What is the tip precentage? ")
+tip  = (bill * rate) /100
 print("Your tip is $"+ str(tip))
 print("Your total is ${0:.2f}".format(float(bill)+ float(tip)))
