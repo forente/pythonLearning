e663409757db6a25f099c1eab1f55d841c6b8194
forente <zerofox16@gmail.com>
2019-06-27 08:22:27 -0400

    Added Rect Area

---
+++ rectArea.py
@@ -0,0 +1,8 @@
+width = input("What is the Width of the box in ft? ")
+height = input("What is the Height of the box in ft? ")
+
+print("You entered the Dimention of "+ height+ "ft by "+width +"ft " )
+print("The Area is ")
+
+areaFt = int(width) * int(height)
+print(str(areaFt)+ " Square ft")
